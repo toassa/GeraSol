998977164e729b79a7b6115b5e3f6bc576248898
Raissa Toassa <raissa.toassa@unesp.br>
2022-09-08 17:36:35 -0300

    08.09.2022
    Desenvolvemos:
    - Rodapé
    - Páginas (Cabeçalho)
    - Ícones (JS)
    
    Co-Authored-By: peraaaaaa <112434344+peraaaaaa@users.noreply.github.com>

---
+++ site_gera/utils/js.js
@@ -0,0 +1,23 @@
+function corzinha(){ 
+    var pagat = window.location.href;
+    var index = "http://200.145.153.91/raissamartinelli/php/site_gera/index.html";
+    var historia = "http://200.145.153.91/raissamartinelli/php/site_gera/utils/historia.html";
+    var projetos = "http://200.145.153.91/raissamartinelli/php/site_gera/utils/projetos.html";
+    var contato= "http://200.145.153.91/raissamartinelli/php/site_gera/utils/contato.html";
+    if (index==pagat){
+        document.getElementById("home_button").style.color="white";
+    }
+    if (historia==pagat)
+    {
+        document.getElementById("history_button").style.color="white";
+    }
+    if (projetos==pagat)
+    {
+        document.getElementById("products_button").style.color="white";
+    }
+    if (contato==pagat)
+    {
+        document.getElementById("contact_button").style.color="white";
+    }
+//https://codepen.io/laugusta/pen/ZBjzVW
+}
