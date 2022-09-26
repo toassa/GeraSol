0aae560f432b4fefcf52cbf6cc8418805346b868
Raissa Toassa <raissa.toassa@unesp.br>
2022-09-26 15:34:54 -0300

    26.09.2022
    - Home: 9 quadrados
    - Arrumação do contato

--- site_gera/utils/contato.php
+++ site_gera/utils/contato.php
@@ -15,10 +15,11 @@
                 "X=Mailer:PHP/".phpversion();
 
     if(mail($to, $subject, $body, $header)){
-        //header("location:G2_carrinho_front.php");
-        echo "<script type='text/javascript'>alert('E-mail enviado com sucesso!')</script>";  
+        echo "<script type='text/javascript'>alert('E-mail enviado com sucesso!')</script>";
+        header("location:contato.html");
     }else{
         echo "<script type='text/javascript'>alert('Houve um erro durante o envio do e-mail')</script>";  
+        header("location:contato.html");
     }
 
 ?>
