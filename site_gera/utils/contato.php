ce39f8e48807bd01ab8a9cd7121ca0338bd25813
Raissa Toassa <raissa.toassa@unesp.br>
2022-09-29 17:54:39 -0300

    28.09.2022
    - Testes e arrumações da página de contato (a arrumar)
    - Início da página "Sobre Nós"
    - Arrumação do index
    
    Co-Authored-By: peraaaaaa <112434344+peraaaaaa@users.noreply.github.com>

--- site_gera/utils/contato.php
+++ site_gera/utils/contato.php
@@ -1,25 +1,20 @@
 <?php
+$emailcontato=$_POST['projeto.gerasol@gmail.com'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$titulo =$_POST['titulo'];
+$conteudo = $_POST['conteudo'];
 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $titulo = $_POST['titulo'];
-    $conteudo = $_POST['conteudo'];
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: $nome <$email>';
 
-    $to = "projeto.gerasol@gmail.com";
-    $subject = "Contato Site Gera - ". $titulo;
-    $body = "Nome: ". $nome. "\n".
-            "Email: ". $email. "\n".
-            "Mensagem: ". $conteudo;
-    $header = "From:".$email."\n".
-                "Reply-To:".$email."\n".
-                "X=Mailer:PHP/".phpversion();
 
-    if(mail($to, $subject, $body, $header)){
-        echo "<script type='text/javascript'>alert('E-mail enviado com sucesso!')</script>";
-        header("location:contato.html");
-    }else{
-        echo "<script type='text/javascript'>alert('Houve um erro durante o envio do e-mail')</script>";  
-        header("location:contato.html");
-    }
-
+if(mail($emailcontato, $titulo, $conteudo, $headers))
+{
+    echo"<script>alert('EMAIL ENVIADO')</script>";
+}
+else{
+    echo"<script>alert('EMAIL NAO ENVIADO')</script>";
+}
 ?>
